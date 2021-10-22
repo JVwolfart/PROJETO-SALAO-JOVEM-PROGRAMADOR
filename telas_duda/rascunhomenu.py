05681f78dafdb99a9d5c03ac76e224ff7e2c5198
JVwolfart <jvitor@pinheirasc.com>
2021-10-22 14:11:53 -0300

    JV

--- telas_duda/rascunhomenu.py
+++ telas_duda/rascunhomenu.py
@@ -9,7 +9,7 @@ from PyQt5 import uic,QtWidgets
 if __name__ == '__main__':
 
     qt = QtWidgets.QApplication(sys.argv)
-    menu = uic.loadUi('tela-menu.ui')
+    menu = uic.loadUi('telas_duda/tela-menu.ui')
 
     menu.show()
     qt.exec_()
